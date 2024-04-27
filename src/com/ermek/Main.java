47ce559dd6950183a27c8861c9ddc7028da35434
ermechaP <163899553+ermechaP@users.noreply.github.com>
2024-04-27 10:15:59 -0500

    Update Main.java

--- src/com/ermek/Main.java
+++ src/com/ermek/Main.java
@@ -1,6 +1,6 @@
 package com.ermek;
 import com.ermek.movie.Movies;
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
  }
 }
